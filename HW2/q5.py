71bb528bb53b8fff358c90b440d9ed424df8bcc7
Jamie <rjlyman03@tuta.com>
2026-02-27 22:15:19 -0500

    unpleasing end

--- HW2/q5.py
+++ HW2/q5.py
@@ -1,4 +1,8 @@
 import numpy as np
+from libwind import kaimal_omega
+from libsignal import psd
+import matplotlib.pyplot as plt
+from scipy.integrate import trapezoid
 
 def spectrum2TimeSeries(S_Xw, omegas):
     PI = np.pi
@@ -18,8 +22,28 @@ def spectrum2TimeSeries(S_Xw, omegas):
     for i in range(N_plus+1):
         if i==0:
             X[0] = N*np.sqrt(S_Xw[0]*dw)
-        A[i] = np.sqrt(-2*S_Xw[i]*dw*np.log(U[i]))
+        A[i] = np.sqrt(2*S_Xw[i]*dw)*np.sqrt(-1*np.log(U[i]))
         X[i] = 0.5*N*A[i]*np.exp(1j*phi[i])
     x = np.fft.irfft(X, N)
     return x, t
+
+U0 = 11
+sigma = 3
+L = 100
+f = 5*np.array(range(30))
+omegas = 2*np.pi*f
+S_test = kaimal_omega(omegas, U0, sigma, L)
+x_test,t_test = spectrum2TimeSeries(S_test, omegas)
+sigma_squared = np.var(x_test)
+freqs, S_Xf = psd(t_test, x_test)
+integral = trapezoid(freqs, S_Xf)
+print(freqs)
+print("Integral of PSD: ", integral)
+print("Variance: ", sigma_squared)
+plt.plot(t_test, x_test)
+plt.xlabel('Time')
+plt.ylabel('Turbulence')
+plt.title('Kaimal Process Realization from Spectrum')
+plt.show()
+
 
