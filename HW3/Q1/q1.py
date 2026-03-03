54334f82d842e69ab86bf42b5be8c25ba019284a
Jamie <rjlyman03@tuta.com>
2026-03-03 13:23:44 -0500

    skeleton of HW

---
+++ HW3/Q1/q1.py
@@ -0,0 +1,7 @@
+# Import Statements
+
+if _name_ == '_main_':
+    # Code here is not executed in main
+
+# Code
+
---
+++ HW3/hw3-main.py
@@ -0,0 +1,8 @@
+# Morgan
+from Q1 import q1
+from Q2 import q2
+# Jamie
+from Q3 import q3
+# Nate
+from Q4 import q4
+from Q5 import q5
