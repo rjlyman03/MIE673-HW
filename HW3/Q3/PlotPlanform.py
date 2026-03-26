01c135700d8b3f0ab0124b3facd6bdfed67a0363
Jamie <rjlyman03@tuta.com>
2026-03-25 21:56:53 -0400

    problem 3 - Incomplete Bonus

---
+++ HW3/Q3/PlotPlanform.py
@@ -0,0 +1,164 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['font.size'] = 13
+
+
+def planform_nowakerot(r, R, TSR, Cl, B=3):
+    """ 
+    Chord and twist distribution for ideal rotor without wake rotation
+
+    INPUTS:
+     - r   (array): radial positions [m]tj
+     - R   (float): rotor radius [m] 
+     - TSR (float): design tip-speed ratio=Omega R/U0 [-]
+     - Cl  (float): design lift coefficient [-]
+     - B   (int  ): number of blades [-]
+
+    OUTPUTS:
+     - chord (array): chord [m] (shape of r)
+     - phi   (array): twist [def] (shape of r)
+     - a     (array): axial induction [-] (shape of r)
+     - ap    (array): tangential induction [-] (shape of r)
+    """
+    r = np.asarray(r)
+    lambda_r = TSR*r/R   
+    phi   = np.arctan(2/(3*lambda_r))
+    a     = np.ones_like(r)/3
+    chord = 8*np.pi*r*np.sin(phi)/(3*B*Cl*lambda_r)
+    ap    = r*0
+
+    phi *= 180/np.pi            # [deg]
+    return chord, phi, a, ap
+
+
+def planform_wakerot(r, R, TSR, Cl, B=3):
+    """ 
+    Chord and twist distribution for ideal rotor with wake rotation
+
+    INPUTS:
+     - r   (array): radial positions [m]
+     - R   (float): rotor radius [m] 
+     - TSR (float): design tip-speed ratio=Omega R/U0 [-]
+     - Cl  (float): design lift coefficient [-]
+     - B   (int  ): number of blades [-]
+
+    OUTPUTS:
+     - chord (array): chord [m] (shape of r)
+     - phi   (array): twist [def] (shape of r)
+     - a     (array): axial induction [-] (shape of r)
+     - ap    (array): tangential induction [-] (shape of r)
+    """
+    # TODO
+    lambda_r = TSR*r/R
+    phi = (2/3)*np.arctan(1/lambda_r)
+    print(phi)
+    chord = 8*np.pi*r*(1 - np.cos(phi))/(B*Cl) 
+    sigma = B*chord/(2*np.pi*r)
+    a     = 1/(1 + (4*np.sin(phi)**2)/(sigma*Cl*np.cos(phi)))
+    ap    = (1 - 3*a)/(4*a - 1)
+    phi *= 180/np.pi            # [deg]
+    return chord, phi, a, ap
+
+
+def planform_ClCd(r, R,  TSR, Cl, Cd, B=3):
+    """ 
+    For Grad/Groups
+    """
+    lambda_r = TSR*r/R
+
+	# Axial induction
+    bValid = lambda_r>0
+    a = np.ones_like(r) * 1/4 # Initialization, with value for lambda_r ==0
+    a[bValid] = 1/2 * (1 - np.sqrt(1 + lambda_r[bValid]**2) * np.sin((1/3)*np.arctan(1/lambda_r[bValid])))
+ 	
+ 	# Tangential induction
+    ap = np.full_like(lambda_r, np.nan)  # Initialization, with value for lambda_r ==0
+    ap[bValid] = (1 - 3*a[bValid])/(4*a[bValid] - 1)
+
+ 	# Flow angle and chord accounting for tip loss and drag
+    phi   = np.arctan((1 - a)/((1 + ap)*lambda_r))
+    Cn    = np.cos(phi)*Cl + np.sin(phi)*Cd # TODO
+    F     = (2/np.pi)*np.arccos(np.exp(-B*(R-r)/(2*r*np.sin(phi))))
+    chord = a*(8*np.pi*r*F*np.sin(phi)**2)/((1-a)*B*Cn) # TODO
+    phi *= 180/np.pi            # [deg]
+    return chord, phi, a, ap 
+
+
+if __name__ == '__main__':
+    B            = 3   # Number of blades
+    TSR          = 11  # Design tip speed ratio [-]
+    alpha_design = 10  # Design angle of attack [deg]
+    R            = 185 # Total tip radius [m]
+    r_hub        = 5   # Hub radius [m]
+    r   = np.linspace(r_hub, R, 100)
+
+    Cl    = 1.51 # based on polar
+    Cd    = 0.14 # based on polar
+
+    # No Wake Rot
+    chord_nw, phi_nw, a_nw, ap_nw = planform_nowakerot(r, R, TSR, Cl, B)
+    twist_nw = phi_nw-alpha_design
+    # Wake Rot
+    chord_wr, phi_wr, a_wr, ap_wr = planform_wakerot(r, R, TSR, Cl, B)
+    twist_wr = phi_wr-alpha_design
+
+    # Wake Rot and Tip Loss
+    chord_ld, phi_ld, a_ld, ap_ld = planform_ClCd(r, R, TSR, Cl, Cd*10, B)
+    twist_ld = phi_ld-alpha_design
+
+    # --- Plot
+    fig,axes = plt.subplots(2, 2, sharex=True, figsize=(12.8,4.8))
+    fig.subplots_adjust(left=0.09, right=0.99, top=0.97, bottom=0.12, hspace=0.30, wspace=0.25)
+    
+    # Chord
+    ax = axes[0,0]
+    ax.plot(r/R, chord_nw/np.max(r) ,'-'  , label='Without wake rotation')
+    ax.plot(r/R, chord_wr/np.max(r) ,'--' , label='With wake rotation')
+    ax.plot(r/R, chord_ld/np.max(r) ,'.'  , label='With wake rotation + Drag + Tiploss')
+    ax.set_ylabel(r'Blade chord, $c/R$ [-]       ')
+    ax.legend()
+    
+    # Twist
+    ax = axes[0,1]
+    ax.plot(r/R, twist_nw, '-'  , label='Without wake rotation')
+    ax.plot(r/R, twist_wr, '--' , label='With wake rotation')
+    ax.plot(r/R, twist_ld ,'.'  , label='With wake rotation + Drag + Tiploss')
+    ax.set_ylabel('Blade twist angle [deg]')
+    
+    # Axial induction
+    ax = axes[1,0]
+    ax.plot(r/R, a_nw, '-'  , label='Without wake rotation')
+    ax.plot(r/R, a_wr, '--' , label='With wake rotation')
+    ax.plot(r/R, a_ld ,'.'  , label='With wake rotation + Drag + Tiploss')
+    ax.set_xlabel(r'Blade radius, $r/R$ [-]')
+    ax.set_ylabel('Axial induction [-]')
+    
+    # Tangential induction
+    ax = axes[1,1]
+    ax.plot(r/R, ap_nw, '-'  , label='Without wake rotation')
+    ax.plot(r/R, ap_wr, '--' , label='With wake rotation')
+    ax.plot(r/R, ap_ld ,'.'  , label='With wake rotation + Drag + Tiploss')
+    ax.set_xlabel(r'Blade radius, $r/R$ [-]')
+    ax.set_ylabel('Tangential induction [-]')
+
+    # --- add ref turbines
+    #from openfast_toolbox.io import FASTInputFile
+    #import os
+    #scriptDir = os.path.dirname(__file__)
+    #stys = ['-','--',':','.-','o']
+    #vP_rated = [15, 22]
+    #for i, pr in enumerate(vP_rated): 
+    #    folder='{:04.1f}MW'.format(pr)
+    #    df = FASTInputFile(os.path.join(scriptDir, '../../data-ref/'+folder+'/AeroDyn_blade.dat')).toDataFrame()
+    #    # --- Plot
+    #    r1 = df['BlSpn_[m]']
+    #    c1 = df['BlChord_[m]']
+    #    t1 = df['BlTwist_[deg]']
+    #    axes[0,0].plot(r1/np.max(r1), c1/np.max(r1), stys[i], label='{:.2f}MW'.format(pr))
+    #    axes[0,1].plot(r1/np.max(r1), t1, stys[i], label='{:.2f}MW'.format(pr))
+    #axes[0,0].legend()
+    #axes[0,1].legend()
+    #fig.savefig('planforms.png')
+
+    plt.show()
--- HW3/Q3/q3.py
+++ HW3/Q3/q3.py
@@ -1,7 +1,49 @@
-# Import Statements
+## Import Statements
+from scipy.interpolate import interp1d 
+import numpy as np
+import pandas as pd
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     # Code here is not executed in main
+    print("testing...")
+## Code
+B = 3 # Number of blades
+lamda_design = 11
+R = 185 # [m] radius
+alpha_design = 10 # ax
 
-# Code
+## Section A
+df = pd.read_csv('../data/ffa-w3-211_Re10M.csv') # load into dataframe
+df.columns = ['alpha', 'C_l', 'C_d', 'C_m']
+
+x = df['alpha'].values # shape inputs 
+y = df[['C_l', 'C_d', 'C_m']].values # shape inputs
+fPolar = interp1d(x, y, axis=0) # interpolant
+
+C_l_design, C_d_design, C_m_design = fPolar(alpha_design)
+print(f"{C_l_design=:.3f}, {C_d_design=:.3f}, and {C_m_design=:.3f}")
+
+## Section D
+## PSUEDO CODE
+def optiaml_aero(Cd_data, Cl_data, alpha_data, r):
+    idx = 0
+    L_over_D = r*0
+    Cl = r*0
+    Cd = r*0 
+    alpha = r*0
+    for ri in r:
+        Cl_r_data = Cl_data[idx,:]
+        Cd_r_data = Cd_data[idx,:]
+        alpha_r_data = alpha_data[idx,:]
+        L_over_Ds= Cl_r_data/Cd_r_data
+        alpha_idx = np.argmax(L_over_Ds)
+        Cl[idx] = Cl_r_data[alpha_idx]
+        Cd[idx] = Cd_r_data[alpha_idx]
+        alpha[idx] = alpha_r_data[alpha_idx]
+        idx += 1
+    return Cl, Cd, alpha
+
+def planform_variable_aero(Cl, Cd, alpha, root_c, max_c):
+    
+    return chord, phi, a, ap
 
