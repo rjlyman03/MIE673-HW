b4ce408ce38eb3a64ca703abb07a23a796e23870
Jamie <rjlyman03@tuta.com>
2026-03-28 22:50:00 -0400

    reorg

--- HW3/Q5/q5.py
+++
@@ -1,7 +0,0 @@
-# Import Statements
-
-if _name_ == '_main_':
-    # Code here is not executed in main
-
-# Code
-
--- HW3/hw3-main.py
+++
@@ -1,8 +0,0 @@
-# Morgan
-from Q1 import q1
-from Q2 import q2
-# Jamie
-from Q3 import q3
-# Nate
-from Q4 import q4
-from Q5 import q5
