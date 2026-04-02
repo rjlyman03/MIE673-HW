d20f95a0104448d96721d7b1186cfa5c92dc152a
unknown <nate.ch.fay@gmail.com>
2026-04-02 12:12:30 -0400

    initial outline

--- HW4/HW4_script.py
+++ HW4/HW4_script.py
@@ -6,3 +6,43 @@ from openfast_toolbox.io import FASTInputFile
 import numpy as np
 import math
 import pandas as pd
+from libbeam import deflection,compute_modes,generalized_MK
+from e45_Sim import statespace,calcOutput
+
+#this initial format is just to organize outputs without running everything every time,
+#feel free to change
+
+#set to 1 to run all problems
+run_problem = 1
+
+if run_problem==2 or run_problem==1:
+    print("Problem 2:")
+    #Problem 2  -----------
+    
+    
+    
+    
+if run_problem==3 or run_problem==1:
+    print("Problem 3:")
+    #Problem 3  -----------
+    
+
+
+
+if run_problem==4 or run_problem==1:
+    print("Problem 4:")
+    #Problem 4  -----------
+    
+    
+
+if run_problem==5 or run_problem==1:
+    print("Problem 5:")
+    #Problem 5  -----------
+
+    
+    
+    
+    
+    
+
+
