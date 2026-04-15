9432dd06410c232b236d7e8d0202315483f8bb81
Jamie <rjlyman03@tuta.com>
2026-04-15 14:03:53 -0400

    J_DT

--- HW4/HW4_library.py
+++ HW4/HW4_library.py
@@ -14,6 +14,10 @@ def straight_beam_inertia(z,m):
     #m is an array of linear mass densitys corresponding to z (kg/m)    
     M = np.trapezoid(m, z)
     S = np.trapezoid(m*z, z)
-    J = np.trapezoid(m*(z**2),z)
+    J = np.trapezoid(m*(z**2), z)
     return M , S , J
+
+def drive_train_inertia(n, J_h, J_g):
+    J_dt = J_h + J_g * (n**2)
+    return J_dt
 
