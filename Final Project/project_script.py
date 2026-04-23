da1399293f3d5dd0108643780f2c9376f967ca87
unknown <nate.ch.fay@gmail.com>
2026-04-23 13:38:55 -0400

    ...

--- Final Project/project_script.py
+++ Final Project/project_script.py
@@ -5,3 +5,20 @@ from openfast_toolbox.io import FASTInputFile
 import numpy as np
 import math
 import pandas as pd
+
+#wind
+import numpy as np
+
+months = [
+    "January","February","March","April","May","June",
+    "July","August","September","October","November","December","Annual"
+]
+
+# Normal Monthly Average Speed [MPH]
+avg_speed_mph = np.array([
+    45.6, 44.6, 39.8, 35.6, 29.6, 26.8,
+    25.5, 24.5, 27.6, 35.5, 39.4, 44.0, 34.9
+], dtype=float)
+
+U_avg = np.mean(avg_speed_mph)
+print(f"Yearly Average Wind Speed = {U_avg:,.4} mph " )
